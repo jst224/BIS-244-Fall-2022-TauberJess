554f2a5686160065baa65bee33a8f943205353ad
Jess <jst224@lehigh.edu>
2022-11-03 14:50:17 -0400

    assignment 4

---
+++ Assignment4.R
@@ -0,0 +1,31 @@
+install.packages("gridExtra")
+library(here)
+library(tidyverse)
+
+library(readr)
+WOT <- read.csv("WOT.csv")
+
+WOT_updated <- filter(WOT, Battles > 3)
+WOT_updated$WR <- sub("%","",WOT_updated$WR)
+WOT_updated$WR <- as.numeric(WOT_updated$WR)
+
+p <- ggplot(data = WOT_updated,
+            mapping = aes(x = reorder(Nation, WR, na.rm=TRUE), y = WR))
+plot_a <- p + geom_boxplot() +
+  labs(x = "Nation Played",
+       y = "Win Rate (WR)",
+       title = "Stats by Nation Played")
+
+q <- ggplot(data = WOT_updated,
+            mapping = aes(x = reorder(Class, WR, ma.rm=TRUE), y = WR))
+
+plot_b <- q + geom_boxplot() +
+  labs(x = "Type Played",
+       y = "Win Rate (WR)",
+       title = "Stats by Type Played")
+
+gridExtra::grid.arrange(
+  plot_a,
+  plot_b,
+  ncol = 2
+)
